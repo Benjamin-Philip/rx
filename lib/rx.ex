25f97cc77027852e87017b0516e347239d130406
Benjamin Philip <benjamin.philip495@gmail.com>
2025-06-17 17:57:59 +0530

    Remove sample code and tests

--- lib/rx.ex
+++ lib/rx.ex
@@ -2,17 +2,4 @@ defmodule Rx do
   @moduledoc """
   Documentation for `Rx`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Rx.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/rx_test.exs
+++ test/rx_test.exs
@@ -1,8 +1,3 @@
 defmodule RxTest do
   use ExUnit.Case
-  doctest Rx
-
-  test "greets the world" do
-    assert Rx.hello() == :world
-  end
 end
